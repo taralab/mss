f55ddf8a425dc2cfacf55e63a1a5948c50427612
EmmTarSystem <manu974.training@gmail.com>
2025-05-18 13:20:43 +0200

    planning : mise en évidence du jours en cours

--- scripts/planning.js
+++ scripts/planning.js
@@ -8,7 +8,16 @@ let userPlanningArray = {
     "vendredi" : ["NATATION"],
     "samedi" : ["RENFORCEMENT"],
     "dimanche" : []
-}
+},
+dayReferences = [
+    "dimanche",
+    "lundi",
+    "mardi",
+    "mercredi",
+    "jeudi",
+    "vendredi",
+    "samedi"
+];
 
 
 
@@ -24,10 +33,26 @@ function onOpenMenuPlanning(){
 // Remplit la grille du planning selon la variable
 function onSetPlanningItems(){
 
+    let daysWeek = dayReferences[new Date().getDay()];//le jour de la semaine
+
     Object.keys(userPlanningArray).forEach(key =>{
-        // Référence le parent et reset ses éléments
-        let parentRef = document.getElementById(`divPlanningContent_${key}`);
-        parentRef.innerHTML = "";
+
+        // Référence la div du jours de la semaine
+        let divWeekDayRef = document.getElementById(`divPlanning_${key}`);
+
+        // traitement css de la div du jours
+        if (key === daysWeek){
+            divWeekDayRef.classList.add("planning-today");
+        }else{
+            if (divWeekDayRef.classList.contains("planning-today")) {
+                divWeekDayRef.classList.remove("planning-today");
+            }
+        }
+
+
+        // Référence le parent des contenus d'activité et reset ses éléments
+        let parentActivityAreaRef = document.getElementById(`divPlanningContent_${key}`);
+        parentActivityAreaRef.innerHTML = "";
 
         if (userPlanningArray[key].length > 0) {
             // pour chaque activité du jour concernée
@@ -37,13 +62,13 @@ function onSetPlanningItems(){
                 newImg.src = activityChoiceArray[activity].imgRef;
 
                 // et l'insère
-                parentRef.appendChild(newImg);
+                parentActivityAreaRef.appendChild(newImg);
             });
         }else{
             // si vide, met l'icone de repos
             let newImg = document.createElement("img");
             newImg.src = "./images/icon-repos.webp";
-            parentRef.appendChild(newImg);
+            parentActivityAreaRef.appendChild(newImg);
         }
 
 
